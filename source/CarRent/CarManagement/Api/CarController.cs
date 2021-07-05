cb86ed24281514f4c445f41a982a06085e74e5b9
Eric Luechinger <ericmarcel.luechinger.gmail.com@zbw-online.ch>
2021-07-05 19:41:45 +0200

    New API

--- source/CarRent/CarManagement/Api/CarController.cs
+++ source/CarRent/CarManagement/Api/CarController.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
-namespace CarRent.Controllers
+namespace CarRent.CarManagement.Api
 {
     [Route("api/[controller]")]
     [ApiController]
--- source/CarRent/CarManagement/Api/CarDto.cs
+++ source/CarRent/CarManagement/Api/CarDto.cs
@@ -1,11 +1,47 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
 namespace CarRent.CarManagement.Api
 {
-    public class CarDto
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarDto : ControllerBase
     {
+        // GET: api/<CarDto>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<CarDto>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<CarDto>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<CarDto>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<CarDto>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
     }
 }
