058f3058ce00297c463ff8fdd0980eaa7af85cd3
Eric Luechinger <ericmarcel.luechinger.gmail.com@zbw-online.ch>
2021-07-20 17:57:09 +0200

    Creating Context

---
+++ source/CarRent/Common/Infrastructure/Context/BaseDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CarRent.Common.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarRent.Common.Infrastructure.Context
+{
+    public abstract class BaseDbContext : DbContext
+    {
+    }
+}
