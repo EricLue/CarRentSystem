656fadb10cbe4f78faa0611b7438dfb43ca894e7
Eric Luechinger <ericmarcel.luechinger.gmail.com@zbw-online.ch>
2021-07-23 11:07:13 +0200

    Creating ReservationRepository and -Dto

---
+++ source/CarRent/ReservationManagement/Infrastructure/ReservationRepository.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CarRent.Common.Interfaces;
+using CarRent.Common.Infrastructure.Context;
+using CarRent.ReservationManagement.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarRent.ReservationManagement.Infrastructure
+{
+    public class ReservationRepository : IRepository<Reservation, Guid>
+    {
+        private readonly CarRentDbContext _carRentDbContext;
+
+        public ReservationRepository(CarRentDbContext carRentDbContext)
+        {
+            _carRentDbContext = carRentDbContext;
+        }
+
+        public List<Reservation> FindEntityById(Guid id)
+        {
+            return _carRentDbContext.Reservations.Include(e => e.Car).Include(e => e.Customer).Where(e => e.Id.Equals(id)).ToList();
+        }
+
+        public List<Reservation> GetAllEntities()
+        {
+            return _carRentDbContext.Reservations.Include(e => e.Car).Include(e => e.Customer).ToList();
+        }
+
+        public void Insert(Reservation entity)
+        {
+            _carRentDbContext.Add(entity);
+            _carRentDbContext.SaveChanges();
+        }
+
+        public void Remove(Reservation entity)
+        {
+            Remove(entity);
+        }
+
+        public void RemoveById(Guid id)
+        {
+            RemoveById(id);
+        }
+
+        public void Update(Reservation entity)
+        {
+            _carRentDbContext.Update(entity);
+            _carRentDbContext.SaveChanges();
+        }
+    }
+}
---
+++ source/CarRent/ReservationManagement/Model/ReservationDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarRent.ReservationManagement.Model
+{
+    public class ReervationDto
+    {
+        public Guid Id { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+        public string State { get; set; }
+        public int DaysTotal { get; set; }
+        public Guid CarId { get; set; }
+        public string CarName { get; set; }
+        public decimal Cost { get; set; }
+        public Guid CustomerId { get; set; }
+        public string CustomerName { get; set; }
+    }
+}
